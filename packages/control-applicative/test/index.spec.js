9da07c712b1bf039a409b726d9a08bc304a350c8
Yoann Sitbon <sitbon.yoann@gmail.com>
2020-01-18 14:18:12 +0000

    add applicative unit test for protocol

--- packages/control-applicative/test/index.spec.js
+++ packages/control-applicative/test/index.spec.js
@@ -1,5 +1,8 @@
-import {extension}                from "@prelude/protocol";
-import {Applicative, apply, pure} from "../src/protocol.js";
+import {expect}                    from "chai";
+import {spyMethod, resetSpy, restoreSpy} from "@prelude/test-spies";
+import {extension}                 from "@prelude/protocol";
+import {Functor}                   from "@prelude/functor";
+import {Applicative, apply, pure}  from "../index.js";
 
 function Identity(value) {
   if (undefined === new.target)
@@ -15,17 +18,36 @@ extension(Identity.prototype, {
   [Applicative.pure](x) {
     return Identity(x);
   },
-  [Applicative.apply](fx) {
-    return this[Applicative.pure](this.value(fx.value));
+  [Applicative.apply](ax) {
+      console.log("CLAA")
+    return this[Applicative.pure](this.value(ax.value));
   }
 })
 
 describe("@prelude/control-applicative", () => {
-  describe("apply(fn: Applicative<((x: A) => B), scope: Applicative<A>): Applicative<B>", () => {
+  describe("pure(F: Constructor<A>, value: T): Applicative<T>", () => {
+    const spy = spyMethod(Identity.prototype, Applicative.pure);
 
+    beforeEach(() => resetSpy(spy));
+
+    it("should call [Applicative.pure]", () => {
+      pure(Identity, 1);
+      expect(spy.called).to.be.true;
+    });
+
+    after(() => restoreSpy(spy));
   });
 
-  describe("pure(F: Constructor<A>, value: T): Applicative<T>", () => {
+  describe("apply(fn: Applicative<((x: A) => B), scope: Applicative<A>): Applicative<B>", () => {
+    const spy = spyMethod(Identity.prototype, Applicative.apply);
 
+    beforeEach(() => resetSpy(spy));
+
+    it("should call [Applicative.apply]", () => {
+      apply(Identity(x => x + 1), Identity(1));
+      expect(spy.called).to.be.true;
+    });
+
+    after(() => restoreSpy(spy));
   });
 });
