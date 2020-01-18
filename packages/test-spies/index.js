a3bd7c61589dc295c9467426051ba6190ee42951
Yoann Sitbon <sitbon.yoann@gmail.com>
2020-01-18 14:14:16 +0000

    Rewrite spies

--- packages/test-spies/index.js
+++ packages/test-spies/index.js
@@ -1,20 +1,44 @@
-export class Spy {
-  constructor() {
-    this.called = false;
-    this.calls  = [];
-  }
+export const resetSpy = self => {
+  self.called = false;
+  self.calls.length = 0;
+}
 
-  on(fn) {
-    return (...args) => {
-      const call = {args: args, returns: fn(...args)};
-      this.called = true;
-      this.calls.push(call);
-      return call.returns;
-    };
-  }
+export const restoreSpy = self => {
+  if (undefined !== self.origin)
+    Object.defineProperty(self.subject, self.member, self.origin);
+  self.subject = undefined;
+  self.origin  = undefined;
+}
 
-  restore() {
-    this.called = false;
-    this.calls.length = 0;
+export const spyFn = fn => {
+  const spy = Spy();
+  return [spy, function(...args) {
+    const call = {args: args, returns: fn.apply(this, args)};
+    spy.called = true;
+    spy.calls.push(call);
+    return call.returns;
+  }];
+}
+
+export const spyMethod = (subject, member) => {
+  const descriptor = Object.getOwnPropertyDescriptor(subject, member);
+  const [spy, value] = spyFn(descriptor.value)
+  Object.defineProperty(subject, member, {...descriptor, value});
+  spy.subject = subject;
+  spy.member  = member;
+  spy.origin  = descriptor;
+  return spy;
+}
+
+function Spy() {
+  if (undefined === new.target) {
+    return new Spy();
+  }
+  else {
+    this.called  = false;
+    this.calls   = [];
+    this.subject = undefined;
+    this.member  = undefined;
+    this.origin  = undefined;
   }
 }
