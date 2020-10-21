6f61239805371f212be9cff1cb91622eed7eabf6
yoann <yoann@askia.com>
2020-10-21 18:40:39 +0200

    Add source documentation to the `@prelude/functor` package

--- packages/functor/lib/trait-impl.js
+++ packages/functor/lib/trait-impl.js
@@ -1,11 +1,27 @@
 import {extension} from "@prelude/trait";
 import {Functor}   from "./trait.js";
 
+/**
+ * Implements the {@link Functor} trait for {@link Array}.
+ * @lends {Array.prototype}
+ */
 extension(Array.prototype, {
-  [Functor.map](f) {
+  /**
+   * Maps all {@link Array} element values into new ones.
+   *
+   * @template A, B
+   * @this A[]
+   * @param {function(A): B} fn
+   * The function called for each elements of this array which returns a new
+   * value to map to the output array.
+   *
+   * @returns {B[]}
+   * Returns another {@link Array} containing the resulting values.
+   */
+  [Functor.map](fn) {
     const xs = [];
     const l  = this.length;
-    for (let i = 0; i < l; ++i) xs.push(f(this[i]));
+    for (let i = 0; i < l; ++i) xs.push(fn(this[i]));
     return xs;
   }
 });
