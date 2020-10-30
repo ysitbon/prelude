58276b653057f7cc91856750ff5bff357602e5a1
yoann <yoann@askia.com>
2020-10-30 15:34:58 +0100

    remove useless step in link script

--- scripts/link.js
+++ scripts/link.js
@@ -2,7 +2,6 @@
 const {run} = require('./run.js');
 
 run("npm unlink @prelude/babel")
-  .then(_ => run("npm unlink", "./packages/babel"))
   .then(_ => run("npm link", "./packages/babel"))
   .then(_ => run("npm link @prelude/babel"))
   .catch(console.error);
