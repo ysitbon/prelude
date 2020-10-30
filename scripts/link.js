7b9f63c6f7bea0a6fa2669064b4e5a931f266ecf
yoann <yoann@askia.com>
2020-10-30 14:47:16 +0100

    unlink before re-link

--- scripts/link.js
+++ scripts/link.js
@@ -2,7 +2,7 @@
 const {exec}    = require('child_process');
 const {resolve} = require('path');
 const cwd = process.cwd();
-const pbd = resolve(cwd, "./packages/babel");
 
-exec("npm link", {cwd: pbd});
+exec("npm unlink @prelude/babel", {cwd});
+exec("npm link", {cwd: resolve(cwd, "./packages/babel")});
 exec("npm link @prelude/babel", {cwd});
